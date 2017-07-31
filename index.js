3dec39ddb34d6682f767890d0af75cedbea49bb0
N4de <JaAnnala@gmail.com>
2017-07-31 16:50:12 +0300

    add number animation for Clients, Employees and Daily users section

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-    $('.Count').each(function () {
-	  var $this = $(this);
-	  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
-	    duration: 1000,
-	    easing: 'swing',
-	    step: function () {
-	      $this.text(Math.ceil(this.Counter));
-	    }
-	  });
-	});
+	var executed = false;
+	var waypoint = new Waypoint({
+	  element: document.getElementById('count'),
+	  handler: function() {
+	  	if (!executed) {
+	  		executed = true;
+		    $('.count').each(function () {
+			  var $this = $(this);
+			  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+			    duration: 1000,
+			    easing: 'swing',
+			    step: function () {
+			      $this.text(Math.ceil(this.Counter));
+			    }
+			  });
+			});
+	  	}
+	  },
+	  offset: '50%'
+	})
 });
